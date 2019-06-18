32cfc854e571d1fc0f2e891cfc7d28e8bea7e449
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-06-18 07:14:15 +0200

    feat: update to expo 33

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Root } from 'native-base';
+import React from 'react';
+import App from './src/app';
+
+export default function Application() {
+  return (
+    <Root>
+      <App />
+    </Root>
+  );
+}
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Root } from 'native-base';
-import React from 'react';
-import App from './app';
-
-export default () => (
-  <Root>
-    <App />
-  </Root>
-);
